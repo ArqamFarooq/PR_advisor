ff603e9b0c2f9c6d49760842c30e60da2a692906
arqam farooq <arqam.farooq@devsinc.com>
2022-10-31 01:05:48 +0500

    devise views

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :phone_number, presence: true
+  validates :user_type, presence: true
 end
